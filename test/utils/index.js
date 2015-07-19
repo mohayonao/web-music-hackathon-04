f68ff9670b40284ffd721597a68af980a54dc51e
mohayonao <mohayonao@gmail.com>
2015-07-20 07:03:31 +0900

    update utils

--- test/utils/index.js
+++ test/utils/index.js
@@ -23,10 +23,61 @@ describe("utils", () => {
       assert.deepEqual(list, [ 1, 2 ]);
     });
   });
+  describe("dbamp(db: number): number", () => {
+    it("works", () => {
+      assert(utils.dbamp(0) === 1);
+      assert(closeTo(utils.dbamp(-1), 0.89125093813375, 1e-6));
+      assert(closeTo(utils.dbamp(-2), 0.79432823472428, 1e-6));
+      assert(closeTo(utils.dbamp(-3), 0.70794578438414, 1e-6));
+    });
+  });
+  describe("debounce(func: function, wait: number): function", (done) => {
+    it("works", () => {
+      let spy = sinon.spy();
+      let func = utils.debounce(spy, 10);
+
+      setTimeout(() => {
+        func(0);
+        func(1);
+        func(2);
+      }, 0);
+
+      setTimeout(() => {
+        func(3);
+        func(4);
+        func(5);
+      }, 50);
+
+      setTimeout(() => {
+        assert(spy.callCount === 2);
+        assert(spy.args[0][0] === 2);
+        assert(spy.args[1][0] === 5);
+        done();
+      }, 100);
+    });
+  });
   describe("defaults(value: any, defaultValue: any): any", () => {
     it("works", () => {
       assert(utils.defaults(0, 1) === 0);
       assert(utils.defaults(UNDEFINED, 0) === 0);
+    });
+  });
+  describe("finedetune(fine: number): number", () => {
+    it("works", () => {
+      assert(utils.finedetune(0) === 0);
+      assert(closeTo(utils.finedetune(60), 100, 1));
+      assert(closeTo(utils.finedetune(123), 200, 1));
+      assert(closeTo(utils.finedetune(189), 300, 1));
+      assert(closeTo(utils.finedetune(260), 400, 1));
+      assert(closeTo(utils.finedetune(335), 500, 1));
+      assert(closeTo(utils.finedetune(414), 600, 1));
+      assert(closeTo(utils.finedetune(498), 700, 1));
+      assert(closeTo(utils.finedetune(587), 800, 1));
+      assert(closeTo(utils.finedetune(682), 900, 1));
+      assert(closeTo(utils.finedetune(782), 1000, 1));
+      assert(closeTo(utils.finedetune(888), 1100, 1));
+      assert(closeTo(utils.finedetune(1000), 1200, 1e-6));
+      assert(closeTo(utils.finedetune(-1000), -1200, 1e-6));
     });
   });
   describe("getItem(object: object, keys: string[]): any", () => {
@@ -79,6 +130,19 @@ describe("utils", () => {
       assert(func(20, 30) === 30);
       assert(func(30, 40) === 30);
       assert(spy.callCount === 1);
+    });
+  });
+  describe("removeItem(object: object, keys: string[]): any", () => {
+    it("works", () => {
+      let object = { a: { b: { c: 0 } } };
+
+      assert(utils.removeItem(object, []) === object);
+      assert(utils.removeItem(object, [ "a", "b", "c" ]) === 0);
+      assert.deepEqual(object, { a: { b: {} } });
+      assert(utils.removeItem(object, [ "a" ]));
+      assert.deepEqual(object, {});
+      assert(utils.getItem(object, [ "a", "b", "c", "d" ]) === null);
+      assert(utils.getItem(object, [ "a", "c", "d", "c" ]) === null);
     });
   });
   describe("removeIfExists(list: any[], value: any): void", () => {
