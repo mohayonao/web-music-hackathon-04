66cc3821a2fd955b2d3cba458f146524814a8f2d
mohayonao <mohayonao@gmail.com>
2015-07-22 13:03:21 +0900

    update config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,14 @@
 export default {
-  TICKS_PER_BEAT: 120,
+  SONGS: [
+    "prelude",
+    "menuet",
+    "clair-de-lune",
+    "passepied",
+  ],
+  DEFAULT_SONG: "clair-de-lune",
   DEFAULT_PARAMS: [
     0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 64,
+    1, 0, 0, 0, 0, 0, 0, 0,
   ],
 };
