c549411415a6f33f7657417e9ff17a5e31ec4a49
mohayonao <mohayonao@gmail.com>
2015-07-25 10:45:37 +0900

    update sound

--- src/sound/tracks/IkedaTrack.js
+++ src/sound/tracks/IkedaTrack.js
@@ -5,23 +5,23 @@ export default class IkedaTrack extends Track {
   constructor(...args) {
     super(...args);
 
-    this.stutter = this.stutter(1 / 12);
-    this.gate = this.gate(2 / 3);
-    this.filter = this.filter(() => Math.random() < 0.25);
-    this.map = this.map((data) => {
+    this.stutterNode = this.stutter(1 / 12);
+    this.gateNode = this.gate(2 / 3);
+    this.filterNode = this.filter(() => Math.random() < 0.25);
+    this.mapNode = this.map((data) => {
       return utils.xtend(data, {
         noteNumber: (data.noteNumber % 36) + 96,
         program: "SineTone",
       });
     });
 
-    this.pipe(this.stutter).pipe(this.gate).pipe(this.filter).pipe(this.map).pipe(this.output);
+    this.pipe(this.stutterNode).pipe(this.gateNode).pipe(this.filterNode).pipe(this.mapNode).pipe(this.output);
   }
 
   setState(...args) {
     super.setState(...args);
 
-    this.stutter.ticksPerBeat = this.ticksPerBeat;
-    this.stutter.tempo = this.tempo;
+    this.stutterNode.ticksPerBeat = this.ticksPerBeat;
+    this.stutterNode.tempo = this.tempo;
   }
 }
--- src/sound/tracks/Track5_Delay.js
+++ src/sound/tracks/Track5_Delay.js
@@ -1,8 +1,20 @@
 import Track from "../Track";
-// import utils from "./utils";
+import utils from "./utils";
 
 export default class Track5 extends Track {
   constructor(...args) {
     super(...args);
+
+    this.delayNode = this.delay(1 / 8);
+
+    this.pipe(this.delayNode).pipe(this.extend({
+      program: "DelayTone",
+    })).pipe(this.output);
+  }
+
+  setState(...args) {
+    super.setState(...args);
+
+    this.delayNode.feedback = utils.linlin(this.params[5], 0, 127, 0, 0.95);
   }
 }
